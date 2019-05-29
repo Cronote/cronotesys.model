eee1f276fe25f787e123ccad0fe0ce46d6d7689e
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2019-05-29 20:38:31 -0300

    fixing the  loginExists method
    Signed by: Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>

--- src/main/java/com/cronoteSys/model/bo/LoginBO.java
+++ src/main/java/com/cronoteSys/model/bo/LoginBO.java
@@ -52,7 +52,7 @@ public class LoginBO {
 
 	public LoginVO loginExists(String sEmail) {
 		if(RestUtil.isConnectedToTheServer()) {
-			return RestUtil.get("").readEntity(LoginVO.class);
+			return RestUtil.get("email_exists?email="+sEmail).readEntity(LoginVO.class);
 		}
 		return new LoginDAO().loginExists(sEmail);
 	}
