252ac5b73f207b72dca2fa49fed9bb3eb74296e7
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2019-05-29 22:17:28 -0300

    Signed by: Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>

--- src/main/java/com/cronoteSys/util/RestUtil.java
+++ src/main/java/com/cronoteSys/util/RestUtil.java
@@ -17,7 +17,7 @@ public class RestUtil {
 			System.out.println(response);
 //			if (response.contains("SUCCESS")) {
 //				System.out.println("Online");
-				return true;
+			return true;
 
 //			}
 		} catch (Exception e) {
@@ -31,8 +31,8 @@ public class RestUtil {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target(host + link);
 		Response response = target.request(MediaType.APPLICATION_JSON).get();
-		if(response.getStatus()==204)
-			return null;
+		if (response.getStatus() == 204)
+			return Response.noContent().build();
 		return response;
 	}
 
