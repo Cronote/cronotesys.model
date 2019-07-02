e601a03d3c04f66689728ccef3571945309615e0
Hettos <39107945+Hettos@users.noreply.github.com>
2019-07-02 15:51:47 -0300

    Criação do EmailVO

---
+++ src/main/java/com/cronoteSys/model/vo/EmailVO.java
@@ -0,0 +1,32 @@
+package com.cronoteSys.model.vo;
+
+public class EmailVO {
+	private String[] receiver;
+	private String message;
+	private String subject;
+	
+	public EmailVO(String[] receiver, String message, String subject) {
+		this.receiver = receiver;
+		this.message = message;
+		this.subject = subject;
+	}
+	
+	public String[] getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String[] receiver) {
+		this.receiver = receiver;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+}
