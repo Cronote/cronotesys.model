c971d41e4e26db214445701355f0ed566f9e18ab
Hettos <fdmhet@gmail.com>
2019-08-19 23:01:09 -0300

    Corre;'ao do emailBO
    Mudan;a da função do emailBO para o email util dentro da rest API

--- src/main/java/com/cronoteSys/model/bo/EmailBO.java
+++ src/main/java/com/cronoteSys/model/bo/EmailBO.java
@@ -1,44 +1,33 @@
 package com.cronoteSys.model.bo;
 
+import java.net.URLEncoder;
 import java.util.concurrent.CompletableFuture;
 
-import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
 
 import com.cronoteSys.model.vo.EmailVO;
+import com.cronoteSys.util.RestUtil;
+import com.google.gson.Gson;
 
 public class EmailBO {
 	private final String sUser = "cronotesys@gmail.com";
 	private final String sPass = "qfkmpdycmduuzfmr";
 	private final int iSmtpPort = 465;
 	private final String sHostName = "smtp.googlemail.com";
-	public boolean genericEmail(EmailVO emailVO) {
-		try {
-			Email email = new SimpleEmail();
-			email.setHostName(sHostName);
-			email.setSmtpPort(iSmtpPort);
-			email.setAuthenticator(new DefaultAuthenticator(sUser, sPass));
-			email.setSSLOnConnect(true);
-			email.setFrom(sUser,"Cronote");
-			email.setSubject(emailVO.getSubject());
-			email.setMsg(emailVO.getMessage());
-			if(emailVO.getReceiver().length == 0) {
-				System.out.println("Erro, não há destinatario.");
-			}else {
-				String[] receiver = emailVO.getReceiver();
-				for (int i = 0; i < receiver.length; i++) {
-					email.addTo(receiver[i]);
-				}
-			}
-			sendEmail(email);
+	public boolean genericEmail(EmailVO emailVO) throws EmailException {
+		String emailEncoder = URLEncoder.encode(new Gson().toJson(emailVO));
+		System.out.println("send_email?receivers="+emailEncoder);
+		String string =  RestUtil.get("send_email?receivers="+emailEncoder).readEntity(String.class);
+		System.out.println(string+"HE DO THE PONG DANCE");
+		if(string.contains("true")) {
 			return true;
-		} catch (EmailException e) {
-			e.getMessage();
+		}else {
+			return false;
 		}
-		return false;		
-	}
+}
+	
+	
 	
 	public void sendEmail(Email email) {
 		CompletableFuture.runAsync(() ->{
