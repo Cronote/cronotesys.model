407c010853d207fc982087b2d61bfce38e4e5900
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2019-10-09 16:13:36 -0300

    hibernateConfig fixed

--- src/main/java/com/cronoteSys/util/HibernateUtil.java
+++ src/main/java/com/cronoteSys/util/HibernateUtil.java
@@ -5,10 +5,24 @@
  */
 package com.cronoteSys.util;
 
+import java.util.Properties;
+
 import javax.persistence.EntityManager;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import com.cronoteSys.model.vo.ActivityVO;
+import com.cronoteSys.model.vo.CategoryVO;
+import com.cronoteSys.model.vo.ExecutionTimeVO;
+import com.cronoteSys.model.vo.LoginVO;
+import com.cronoteSys.model.vo.ProjectVO;
+import com.cronoteSys.model.vo.TeamUser;
+import com.cronoteSys.model.vo.TeamVO;
+import com.cronoteSys.model.vo.UserVO;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -22,23 +36,93 @@ public class HibernateUtil {
 
 	private static SessionFactory sessionFactory;
 
-	static {
-		try {
-			// Create the SessionFactory from standard (hibernate.cfg.xml)
-			// config file.
-			sessionFactory = new Configuration().configure("hibernate-remote.cfg.xml").buildSessionFactory();
-			entityManager = sessionFactory.createEntityManager();
-		} catch (Throwable ex1) {
-			// Log the exception.
-			System.err.println("Initial SessionFactory creation failed." + ex1);
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
 			try {
-				sessionFactory = new Configuration().configure("hibernate-local1.cfg.xml").buildSessionFactory();
-				entityManager = sessionFactory.createEntityManager();
-			} catch (Exception ex2) {
-				System.err.println("Initial SessionFactory creation failed. >>>>>>>>>>> 2" + ex1);
-				// TODO: handle exception
+				Configuration configuration = new Configuration();
+				// Hibernate settings equivalent to hibernate.cfg.xml's properties
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "org.postgresql.Driver");
+				settings.put(Environment.URL,
+						"jdbc:postgresql://ec2-107-20-251-130.compute-1.amazonaws.com:5432/df7gvg8l8kbuaf");
+				settings.put(Environment.USER, "icowfbullsqzij");
+				settings.put(Environment.PASS, "31befc28665481a9be4b2f8eb4a3cc7c92f552fd19986c420203fbef25d6ffe3");
+				settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+				settings.put(Environment.SHOW_SQL, "true");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				settings.put(Environment.HBM2DDL_AUTO, "update");
+
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(LoginVO.class);
+				configuration.addAnnotatedClass(UserVO.class);
+				configuration.addAnnotatedClass(TeamVO.class);
+				configuration.addAnnotatedClass(CategoryVO.class);
+				configuration.addAnnotatedClass(ProjectVO.class);
+				configuration.addAnnotatedClass(ActivityVO.class);
+				configuration.addAnnotatedClass(ExecutionTimeVO.class);
+				configuration.addAnnotatedClass(TeamUser.class);
+
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
+		return sessionFactory;
+	}
+
+	static {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
+				// Hibernate settings equivalent to hibernate.cfg.xml's properties
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "org.postgresql.Driver");
+				settings.put(Environment.URL,
+						"jdbc:postgresql://ec2-107-20-251-130.compute-1.amazonaws.com:5432/df7gvg8l8kbuaf");
+				settings.put(Environment.USER, "icowfbullsqzij");
+				settings.put(Environment.PASS, "31befc28665481a9be4b2f8eb4a3cc7c92f552fd19986c420203fbef25d6ffe3");
+				settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+				settings.put(Environment.SHOW_SQL, "true");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				settings.put(Environment.HBM2DDL_AUTO, "update");
+
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(LoginVO.class);
+				configuration.addAnnotatedClass(UserVO.class);
+				configuration.addAnnotatedClass(TeamVO.class);
+				configuration.addAnnotatedClass(CategoryVO.class);
+				configuration.addAnnotatedClass(ProjectVO.class);
+				configuration.addAnnotatedClass(ActivityVO.class);
+				configuration.addAnnotatedClass(ExecutionTimeVO.class);
+				configuration.addAnnotatedClass(TeamUser.class);
+
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+//		try {
+//			// Create the SessionFactory from standard (hibernate.cfg.xml)
+//			// config file.
+//			sessionFactory = new Configuration().configure("hibernate-remote.cfg.xml").buildSessionFactory();
+//			entityManager = sessionFactory.createEntityManager();
+//		} catch (Throwable ex1) {
+//			// Log the exception.
+//			System.err.println("Initial SessionFactory creation failed." + ex1);
+//			try {
+//				sessionFactory = new Configuration().configure("hibernate-local1.cfg.xml").buildSessionFactory();
+//				entityManager = sessionFactory.createEntityManager();
+//			} catch (Exception ex2) {
+//				System.err.println("Initial SessionFactory creation failed. >>>>>>>>>>> 2" + ex1);
+//				// TODO: handle exception
+//			}
+//		}
 	}
 
 	public static EntityManager getEntityManager() {
